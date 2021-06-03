489855e227db795de532dd2a61b00483ef2ebef2
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-03 15:48:32 +0300

    2021-06-03 12:48:32

---
+++ MW_EXT_META.class.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MediaWiki\Extension\PkgStore;
+
+use OutputPage, Skin;
+
+/**
+ * Class MW_EXT_META
+ */
+class MW_EXT_META
+{
+  /**
+   * Load resource function.
+   *
+   * @param OutputPage $out
+   * @param Skin $skin
+   *
+   * @return bool
+   */
+  public static function onBeforePageDisplay(OutputPage $out, Skin $skin)
+  {
+    return true;
+  }
+}
